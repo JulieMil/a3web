afea19f7a8f2278d3025c2d751b39798017838f0
areveler <areveler@flemingcollege.ca>
2023-11-28 12:38:52 -0500

    navbarjava

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
 //Alanah code starts here;
-function NavbarButton() {
+function NavbarHome() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
         x.className += " responsive";
     } else {
         x.className = "topnav";
     }
+}
+
+function NavbarSecond() {
+    var x = document.getElementById("SecTopNav");
+    if (x.className === "topnav") {
+         x.className += " responsive";
+    } else {
+        x.className = "topnav";
     }
+}
+
+function NavbarThird() {
+    var x = document.getElementById("ThirdTopNav");
+    if (x.className === "topnav") {
+         x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+function NavbarFourth() {
+    var x = document.getElementById("FourthTopNav");
+    if (x.className === "topnav") {
+         x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+function NavbarFifth() {
+    var x = document.getElementById("FifthTopNav");
+    if (x.className === "topnav") {
+         x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
