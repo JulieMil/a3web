e9c3c43cd984bf2370c9804421c2b750bbe83327
areveler <areveler@flemingcollege.ca>
2023-11-28 11:52:16 -0500

    scriptpagechanges

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+//Alanah code starts here;
+function NavbarButton() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+    }
