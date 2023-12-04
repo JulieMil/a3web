50d3c6fda565db81c1db8c8bcc75c4a4f9c74174
lacobham <146376068+lacobham@users.noreply.github.com>
2023-12-04 10:51:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -12,16 +12,6 @@ function Navbar() {
 //Functions for back to top buttom, inspried from: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
 
 
-let mybutton = document.getElementById("topbutton");
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      mybutton.style.display = "block";
-    } else {
-      mybutton.style.display = "none";
-    }
-  }
 
 
 function topFunction() {
