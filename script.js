36013bf1a95443c5860752c7d870edc173801534
areveler <146376003+areveler@users.noreply.github.com>
2023-12-05 19:36:34 -0500

    comments.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-//Alanah code starts here;
-//Script for navbar when collasped, inprired from: https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
+//Owner: Alanah Reveler;
+//Script for navbar when collasped
+//From: https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
 function Navbar() {
-    var x = document.getElementById("myTopnav");
+    var x = document.getElementById("myTopnav"); 
     if (x.className === "navbar") {
         x.className += " responsive";
     } else {
@@ -9,38 +10,38 @@ function Navbar() {
     }
 }
 
-//Functions for back to top buttom, inspried from: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+//Function for back-to-top button to bring back to the top once clicked
+//From:  https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
 
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   }
 
-//Code for make top button appear once scoll 
-//Done on Dec 4,2023
-//Code inspired from: https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_onscroll2
+//Code for to make back-to-top button appear once scrolled 
+//From: https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_onscroll2
 
 window.onscroll = function() {scrollTopButton()};
 
 function scrollTopButton() {
   if (document.documentElement.scrollTop > 10) {
-    document.getElementById("topbutton").className = "top-show";
+    document.getElementById("topbutton").className = "top-show"; //changes class to top-show where display:block
   } else {
-    document.getElementById("topbutton").className = "top-hide";
+    document.getElementById("topbutton").className = "top-hide"; //keeps class to top-hide where display:none
   }
 }
 
-//Code for review button accordions style 
-//Done on Dec 3,2023
+//Code for review button accordions style drop-down
 //From https://www.w3schools.com/w3css/w3css_accordions.asp 
 function Accordion(id) {
   var x = document.getElementById(id);
-  if (x.className.indexOf("panel-show") == -1) {
-    x.className += " panel-show";
+  if (x.className.indexOf("panel-show") == -1) { 
+    x.className += " panel-show"; //if the class isn't panel-show, switch to panel-show (display:block)
   } else { 
-    x.className = x.className.replace("panel-show", "");
+    x.className = x.className.replace("panel-show", ""); //if class id panel-show switch to default
   }
 }
+//Owner End:Alanah Reveler 
 
 //Jordan's code starts here
 // submit alert function for contact us form
